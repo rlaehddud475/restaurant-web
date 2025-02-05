c15e6a09e0a7acc1723ba0c697372e41aeaef6d8
rlaehddud475 <6070kjh475@naver.com>
2025-02-05 16:06:14 +0900

    .

---
+++ src/app/global/ui/outlines/Header.tsx
@@ -0,0 +1,8 @@
+'use client'
+import React from 'react'
+import { styled } from 'styled-components'
+const StyledHeader = styled.header``
+const Header = () => {
+  return <StyledHeader>사이트 상단영역</StyledHeader>
+}
+export default React.memo(Header)
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,21 @@
-import StyledJsxRegistry from "./registry";
-import "./globals.css";
+import StyledJsxRegistry from './registry'
+import './globals.css'
+import Header from './global/ui/outlines/Header'
+import Footer from './global/ui/outlines/Footer'
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body>
-        <StyledJsxRegistry>{children}</StyledJsxRegistry>
+        <StyledJsxRegistry>
+          <Header />
+          <main>{children}</main>
+          <Footer />
+        </StyledJsxRegistry>
       </body>
     </html>
-  );
+  )
 }
