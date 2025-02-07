aecd6b77afc6bdba81ff1e71e7a456db0ab8562c
rlaehddud475 <6070kjh475@naver.com>
2025-02-07 10:39:15 +0900

    .

--- src/app/member/services/actions.ts
+++ src/app/member/services/actions.ts
@@ -1,13 +1,36 @@
 'use server'
+import { format } from 'date-fns'
 import { redirect } from 'next/navigation'
+
 export const processJoin = async (parms, formData: FormData) => {
-  const redirectUrl = parms?.get('redirectUrl') ?? '/memeber/login'
+  //   const redirectUrl = parms?.get('redirectUrl') ?? '/memeber/login'
+  const redirectUrl = '/member/login'
   const form = {},
     erros = {}
   let hasErrors = false
-  for (const [key, value] of formData.entries()) {
+  for (let [key, value] of formData.entries()) {
+    if (key.includes('$ACTION')) continue
+    if (key === 'birthDt' && value && value.trim()) {
+      value = format(new Date(value), 'yyyy-MM-dd')
+    }
+    if (['false', 'true'].includes(value)) {
+      value = value === 'true'
+    }
     form[key] = value
   }
+  const requiredFields = {
+    email: '이메일을 입력하세요.',
+    name: '이름을 입력하세요.',
+    passwrod: '비밀번호를 입력하세요.',
+    confirmPassword: '비밀번호를 확인하세요.',
+    phoneNumber: '휴대폰번호를 입력하세요.',
+    gender: '성별을 선택하세요',
+    birthDt: '생년월일을 선택하세요',
+    requiredTerms1: '이용약관에 동의하셔야 합니다.',
+    requiredTerms2: '개인정보 처리방침에 동의하셔야 합니다',
+    requiredTerms3: '개인정보 수집 및 이용에 동의하셔야 합니다.',
+  }
+  console.log('form', form)
   redirect(redirectUrl)
 }
 export const processLogin = async (parms, formData: FormData) => {}
